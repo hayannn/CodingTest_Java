c5ede5a4fee0b70815d56676985c1f1dc4b17872
hayan <102213509+hayannn@users.noreply.github.com>
2024-01-30 09:26:51 +0900

    [Bronze II] Title: 단어의 개수, Time: 472 ms, Memory: 30140 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -3,11 +3,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String string = scanner.nextLine().trim();
 
-        if(string.isEmpty())
-            System.out.println('0');
-        else
-            System.out.println(string.split(" ", -1).length);
+        String string = scanner.nextLine();
+
+        StringTokenizer stringTokenizer = new StringTokenizer(string, " ");
+        System.out.println(stringTokenizer.countTokens());
     }
 }
