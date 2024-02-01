decd3999996b9c674e9a2b5d3126a7fb028da211
hayan <102213509+hayannn@users.noreply.github.com>
2024-02-01 17:41:36 +0900

    [Bronze II] Title: 다이얼, Time: 200 ms, Memory: 17656 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,78 +1,28 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String words = scanner.nextLine();
+        String words = scanner.nextLine().toUpperCase();
 
-
-        if(words.length() < 2 || words.length() > 15 || !isAllUpperCase(words)){
-            System.out.print("단어는 2 이상 15이하로 입력해야 하며, 알파벳 대문자만 입력해주세요.");
-        } else {
-            int count = 0;
-            int length = words.length();
-            for (int i = 0; i < length; i++) {
-                switch (words.charAt(i)) { //charAt(string 문자열 중 한글자를 char 타입으로 변환
-                case 'A':
-                case 'B':
-                case 'C':
-                    count += 3;
-                    break;
-
-                case 'D':
-                case 'E':
-                case 'F':
-                    count += 4;
-                    break;
-
-                case 'G':
-                case 'H':
-                case 'I':
-                    count += 5;
-                    break;
-
-                case 'J':
-                case 'K':
-                case 'L':
-                    count += 6;
-                    break;
-
-                case 'M':
-                case 'N':
-                case 'O':
-                    count += 7;
-                    break;
-
-                case 'P':
-                case 'Q':
-                case 'R':
-                case 'S':
-                    count += 8;
-                    break;
-
-                case 'T':
-                case 'U':
-                case 'V':
-                    count += 9;
-                    break;
-
-                case 'W':
-                case 'X':
-                case 'Y':
-                case 'Z':
-                    count += 10;
-                    break;
-                }
-            }
-            System.out.println(count);
+        // 입력된 문자열의 길이가 2글자 이상 15글자 이하인지 확인
+        if (words.length() < 2 || words.length() > 15) {
+            System.out.println("2글자 이상 15글자 이하의 문자열을 입력해야 합니다.");
+            return;
         }
-    }
-    private static boolean isAllUpperCase(String string){
-        for(char c : string.toCharArray()){
-            if(!Character.isUpperCase(c)){
-                return false;
-            }
+
+        int count = 0;
+
+        for (int i = 0; i < words.length(); i++) {
+            char character = words.charAt(i);
+            int wtonumber = character - 'A';
+
+            count += (wtonumber / 3) + 3;
+            if (character == 'S' || character == 'V' || character == 'Y' || character == 'Z') {
+                count -= 1;
+            }               
         }
-        return true;
+        System.out.print(count);
     }
 }
