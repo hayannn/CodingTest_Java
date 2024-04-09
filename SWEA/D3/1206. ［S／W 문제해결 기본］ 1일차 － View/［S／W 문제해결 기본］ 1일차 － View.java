7b9e8baa80ad0f0a0524fc74f8e6ff5ab0501c31
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-04-09 16:15:41 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 195 ms, Memory: 42,148 KB -BaekjoonHub

---
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+    static int n;
+    static int[] array;
+
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int T=10;
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            n = sc.nextInt();
+            array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = sc.nextInt();
+            }
+
+            int count = 0;
+            for (int i = 2; i < n - 2; i++) {
+                int max = Math.max(array[i - 2],
+                        Math.max(array[i - 1],
+                                Math.max(array[i + 1], array[i + 2])));
+                
+                if (array[i] > max)
+                    count += array[i] - max;
+            }
+            System.out.println("#" + test_case + " " + count);
+        }
+    }
+}
