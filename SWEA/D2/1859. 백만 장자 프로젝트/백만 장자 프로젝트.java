43550f2975a139bbcd7bb1021b32f796b7ed188f
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-04-09 16:11:14 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 2,264 ms, Memory: 298,120 KB -BaekjoonHub

---
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.List;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		sc.nextLine();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int n = Integer.parseInt(sc.nextLine());
+			List<Integer> numList = Arrays.stream(sc.nextLine().split(" "))
+					.map(Integer::parseInt)
+					.collect(Collectors.toList());
+			
+			int[] temp = new int[numList.size()];
+			int maxPrice = 0;
+			double result = 0;
+			
+			for(int i=n-1; i>=0; i--) {
+				if(maxPrice < numList.get(i).intValue())
+					maxPrice = numList.get(i).intValue();
+					temp[i] = maxPrice - numList.get(i).intValue();
+			}
+			for(int i=0; i<temp.length; i++)
+				result += temp[i];
+			 System.out.println("#" + test_case + " " + Math.round(result));
+		}
+	}
+}
