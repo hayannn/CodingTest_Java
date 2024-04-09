c7712d13427c84fdfc35cefad995dbae71069780
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-04-09 16:18:17 +0900

    [D2] Title: 파리 퇴치, Time: 159 ms, Memory: 34,468 KB -BaekjoonHub

---
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int [][] arr = new int[n][n];
+			for(int i=0; i <n; i++) {
+				for(int j=0; j<n; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			
+			int max = 0;
+			for(int i=0; i <n-m+1; i++) {
+				for(int j=0; j<n-m+1; j++) {
+					int sum = 0;
+					for(int k=0; k<m; k++) {
+						for(int p=0; p<m; p++) {
+							sum += arr[i+k][j+p];
+						}
+					}
+					if(max<sum)
+						max = sum;
+				}
+			}
+			System.out.println("#" + test_case + " " + max);
+			
+			
+		}
+	}
+}
