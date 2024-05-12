e3d4b60454c4b8cda9f3b4e3468f7707290dd37d
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-05-12 18:18:30 +0900

    [D1] Title: 홀수만 더하기, Time: 134 ms, Memory: 25,964 KB -BaekjoonHub

--- SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
@@ -1,22 +1,21 @@
-import java.util.Scanner;
-import java.io.FileInputStream;
-
+import java.util.*;
+ 
 class Solution
 {
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			int sum = 0;
-			for(int i=0; i<10; i++) {
-				int data = sc.nextInt();
-				if(data%2 == 1)
-					sum += data;
-				}
-			System.out.println("#" + test_case + " " + sum);
-		}
-	}
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+ 
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int sum = 0;
+            for(int i=0; i<10; i++) {
+                int num = sc.nextInt();
+                if(num%2 == 1)
+                    sum += num;
+                }
+            System.out.println("#" + test_case + " " + sum);
+        }
+    }
 }
