23e485e58361b6c916a441db70350e5bd6e75166
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-05-14 15:28:55 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 203 ms, Memory: 45,064 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -2,30 +2,31 @@ import java.util.*;
 
 class Solution
 {
-	public static void main(String args[]) throws Exception
-	{
+    public static void main(String args[]) throws Exception
+    {
 
-		Scanner sc = new Scanner(System.in);
-		int T=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int T=sc.nextInt();
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			sc.nextInt();
-			int[] score = new int[101];
-			 
-			for(int i=0; i<1000; i++) {
-				 score[sc.nextInt()]++;
-			 }
-			
-			 int max=0;
-             int result=0;
-			 for(int i=0; i < score.length; i++) {
-				 if(score[i]>=max) {
-					 max = score[i];
-					 result = i;
-				 }
-			 }
-			 System.out.println("#" + test_case + " " + result);
-		}
-	}
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            sc.nextInt();
+            int[] score = new int[101];
+
+            for(int i=0; i<1000; i++){
+                int one = sc.nextInt();
+                score[one]++;
+            }
+
+            int max = 0;
+            int result = 0;
+            for(int i=0; i<score.length; i++) {
+                if(score[i] >= max) {
+                    max = score[i];
+                    result = i;
+                }
+            }
+            System.out.println("#" + test_case + " " + result);
+        }
+    }
 }
