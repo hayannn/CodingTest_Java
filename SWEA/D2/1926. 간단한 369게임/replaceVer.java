0badf3d5ad16b695532b85bfb1577e670dfca9f4
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-05-16 15:48:48 +0900

    feat: SWEA D2 1926. 간단한 369게임 replace 이용한 풀이 추가

---
+++ SWEA/D2/1926. 간단한 369게임/replaceVer.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		String[] arr = new String[N+1];
+		
+		for(int i=1; i<=N; i++) {
+			
+			arr[i] = String.valueOf(i);
+			
+			 if (arr[i].contains("3") || arr[i].contains("6") || arr[i].contains("9")) {
+				 arr[i]= arr[i].replace("3", "-");
+         arr[i]= arr[i].replace("6", "-");
+         arr[i]= arr[i].replace("9", "-");
+
+         arr[i]= arr[i].replace("0", "");
+         arr[i]= arr[i].replace("1", "");
+         arr[i]= arr[i].replace("2", "");
+         arr[i]= arr[i].replace("4", "");
+         arr[i]= arr[i].replace("5", "");
+         arr[i]= arr[i].replace("7", "");
+         arr[i]= arr[i].replace("8", "");
+			 }
+			 System.out.print(arr[i] + " ");
+		}
+	}
+}
