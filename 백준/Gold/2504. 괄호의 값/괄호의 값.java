a888abea7e109312e564c5e4baba6090d79b72a2
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-05-16 18:29:11 +0900

    [Gold V] Title: 괄호의 값, Time: 216 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String List = sc.nextLine();
+		
+		Stack<Character> stack = new Stack<>();
+		int result = 0;
+		int temp = 1; //임시 값은 괄호가 여는 괄호일 경우 2로, 여는 대괄호일 경우 3으로 초기화
+		boolean isValid = true; //올바른 괄호 쌍이 입력되었는지 여부 확인
+		
+		for(int i=0; i<List.length(); i++) {
+		/*
+			if(List.charAt(i) == '(') openCntS++;
+			
+			if(List.charAt(i) == ')') closeCntS++;
+		
+			if(List.charAt(i) == '[') openCntL++;
+		
+			if(List.charAt(i) == ']') closeCntL++;
+		
+		}
+		
+		if(openCntS == closeCntS && openCntL == closeCntL) {
+			result = (openCntS*2) + ((openCntL+1)*3);
+		} else {
+			result = 0;
+		}
+		*/
+			
+			/*여는 괄호인 경우
+			- 해당 괄호를 스택에 push
+			- temp *2 또는 *3하기
+			*/
+			if(List.charAt(i) == '(') {
+				stack.push(List.charAt(i));
+				temp *= 2;
+			}
+			if(List.charAt(i) == '[') {
+				stack.push(List.charAt(i));
+				temp *= 3;
+			}
+			
+			
+			/*닫는 괄호인 경우
+			 - 스택이 비어있고  + 스택 맨 위에 있는 괄호와 현재 괄호가 매치되지 않는다면(peek로 확인)
+			 -> 올바르지 않은 문자열로 보고 빠져나가기
+			 */
+			if(List.charAt(i) == ')' && (stack.isEmpty() || stack.peek() != '(')) {
+				isValid = false;
+				break;
+			}
+			if(List.charAt(i) == ']' && (stack.isEmpty() || stack.peek() != '[')) {
+				isValid = false;
+				break;
+			}
+			
+			
+			
+			/*올바른 괄호열이 성립되는 경우
+			 - 이전에 여는 괄호가 같은 종류의 괄호였는지 확인 -> 해당 괄호에 할당된 값을 결과에 더하기
+			 - 스택에서 괄호를 pop
+			 - 임시 값 temp를 해당 괄호의 규칙에 맞게 /=2 또는 /=3
+			*/
+			if(List.charAt(i) == ')') {
+				if(List.charAt(i-1) == '(')
+					result += temp;
+				stack.pop();
+				temp /= 2;
+			}
+			if(List.charAt(i) == ']') {
+				if(List.charAt(i-1) == '[')
+					result += temp;
+				stack.pop();
+				temp /= 3;
+			}
+		}
+		
+		
+		
+		
+		if(!isValid || !stack.isEmpty()) { //!(올바른 괄호열) 즉, 올바르지 않은 괄호열이거나 스택이 비어있지 않은 경우 결과는 0 출력
+			result = 0;
+		}
+		System.out.println(result);
+	}
+}
