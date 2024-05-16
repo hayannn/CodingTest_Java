d5912cdcaf83a2cb773a8fe5a90ccfc0a5b99093
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-05-16 15:47:22 +0900

    [D2] Title: 간단한 369게임, Time: 158 ms, Memory: 26,096 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -1,36 +1,33 @@
 import java.util.Scanner;
 import java.io.FileInputStream;
-
-
+ 
+ 
 class Solution
 {
-	public static void main(String args[]) throws Exception
-	{
-
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		
-		String[] arr = new String[N+1];
-		
-		for(int i=1; i<=N; i++) {
-			
-			arr[i] = String.valueOf(i);
-			
-			 if (arr[i].contains("3") || arr[i].contains("6") || arr[i].contains("9")) {
-				 arr[i]= arr[i].replace("3", "-");
-		         arr[i]= arr[i].replace("6", "-");
-		         arr[i]= arr[i].replace("9", "-");
-		
-		         arr[i]= arr[i].replace("0", "");
-		         arr[i]= arr[i].replace("1", "");
-		         arr[i]= arr[i].replace("2", "");
-		         arr[i]= arr[i].replace("4", "");
-		         arr[i]= arr[i].replace("5", "");
-		         arr[i]= arr[i].replace("7", "");
-		         arr[i]= arr[i].replace("8", "");
-			 }
-			 System.out.print(arr[i] + " ");
-		}
-	}
+    public static void main(String args[]) throws Exception
+    {
+ 
+        Scanner sc = new Scanner(System.in);
+         
+        int N = sc.nextInt();
+         
+        String[] arr = new String[N+1];
+         
+        for(int i=1; i<=N; i++) {
+             
+            arr[i] = String.valueOf(i);
+             
+             if (arr[i].contains("3") || arr[i].contains("6") || arr[i].contains("9")) {
+                for(int j=0; j<arr[i].length(); j++) {
+                    if(arr[i].charAt(j) == '3' || arr[i].charAt(j) == '6' || arr[i].charAt(j) == '9') {
+                        System.out.print("-");
+                    }
+                }
+                System.out.print(" ");
+            }
+            else {
+            System.out.print(arr[i] + " ");
+            }
+        }
+    }
 }
