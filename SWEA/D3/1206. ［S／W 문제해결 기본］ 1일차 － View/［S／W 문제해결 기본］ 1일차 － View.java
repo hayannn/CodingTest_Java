123e03d6a8ae10586865a40984d24695f6b5edd4
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-05-17 10:30:57 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 290 ms, Memory: 39,800 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,32 +1,34 @@
-import java.util.*;
- 
+import java.util.Scanner;
+import java.io.FileInputStream;
+
 class Solution
 {
-    static int n;
-    static int[] array;
- 
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-         
-        int T=10;
- 
-        for (int test_case = 1; test_case <= T; test_case++) {
-            n = sc.nextInt();
-            array = new int[n];
-            for (int i = 0; i < n; i++) {
-                array[i] = sc.nextInt();
-            }
- 
-            int count = 0;
-            for (int i = 2; i < n-2; i++) {
-                int max = Math.max(array[i-2],
-                        Math.max(array[i-1],
-                                Math.max(array[i+1], array[i+2])));
-                 
-                if (array[i] > max)
-                    count += array[i] - max;
-            }
-            System.out.println("#" + test_case + " " + count);
-        }
-    }
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = 10;
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int N = sc.nextInt();
+			int[] arr = new int[N];
+			for(int i=0; i<N; i++) {
+				arr[i] = sc.nextInt();
+			}
+			
+			int result = 0;
+			
+			for(int i=2; i<N-2; i++) {
+				
+				int leftMax = Math.max(arr[i-2], arr[i-1]);
+				int rightMax = Math.max(arr[i+1], arr[i+2]);
+				
+				if(arr[i] > leftMax && arr[i] > rightMax) {
+					//result는 현재 건물 높이에서 왼쪽 최댓값과 오른쪽 최댓값 중 큰 값을 뺀 값으로 계산!!
+					result += arr[i] - Math.max(leftMax, rightMax);
+				}
+			}
+			System.out.println("#" + test_case + " " + result);
+		}
+	}
 }
