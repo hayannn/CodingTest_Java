455090ba906ec41791f6201bed02d45a5e7bfb75
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-05-17 09:46:48 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 138 ms, Memory: 26,760 KB -BaekjoonHub

---
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int P = sc.nextInt();
+			int Q = sc.nextInt();
+			int R = sc.nextInt();
+			int S = sc.nextInt();
+			int W = sc.nextInt();
+			
+			int result = 0;
+
+			for(int i=0; i<W; i++) {
+				/*if(W<=R) {
+					result = Q;
+				}
+				else {
+					result = Q + (W-R)*S;
+				}*/
+				result = Math.min(W*P, Math.max(Q,Q + (W-R)*S));
+			}
+			System.out.println("#" + test_case + " " + result);
+		}
+	}
+}
