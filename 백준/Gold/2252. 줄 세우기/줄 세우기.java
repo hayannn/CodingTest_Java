ed38ee3638620327badf010b738609a8b2d40fda
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-05-18 20:35:17 +0900

    [Gold III] Title: 줄 세우기, Time: 1504 ms, Memory: 173244 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,50 +1,56 @@
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int M;
-    static List<Integer>[] adjList;
-    static int[] indegree;
+	
+	static int N;
+	static int M;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+	 static List<Integer>[] List;
+	
+	static int[] indegree;
 
-        adjList = new ArrayList[N + 1];
-        indegree = new int[N + 1];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
 
-        for (int i = 1; i <= N; i++) {
-            adjList[i] = new ArrayList<>();
-        }
+		List = new ArrayList[N+1];
+		indegree = new int[N+1];
+		
+		
+		for (int i = 1; i <= N; i++) {
+			List[i] = new ArrayList<>();
+		}
 
-        for (int i = 0; i < M; i++) {
+        for(int i=0; i<M; i++) {
             int from = sc.nextInt();
             int to = sc.nextInt();
-            adjList[from].add(to);
+            List[from].add(to);
             indegree[to]++;
         }
-
-        topologicalSort();
-    }
-
-    public static void topologicalSort() {
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-
+		bfs();
+	}
+	
+	public static void bfs(){
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        // 진입차수가 0인 노드를 큐에 추가
         for (int i = 1; i <= N; i++) {
             if (indegree[i] == 0) {
-                queue.add(i);
+                pq.add(i);
             }
         }
-
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            System.out.print(current + " ");
-
-            for (int next : adjList[current]) {
-                indegree[next]--;
-                if (indegree[next] == 0) {
-                    queue.add(next);
+        
+        while (!pq.isEmpty()) {
+            int n = pq.poll();
+            System.out.print(n + " ");
+            
+            for (int i : List[n]) {
+                indegree[i]--; // 해당 정점에서 나가는 간선을 제거
+                    
+                if (indegree[i] == 0) {
+                    pq.add(i);
                 }
             }
         }
