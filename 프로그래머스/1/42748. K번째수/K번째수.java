749ac239eab89764c5dee9c340653434dcdd91df
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-05-25 09:47:48 +0900

    [level 1] Title: K번째수, Time: 0.32 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,33 +1,29 @@
-//오전 10시 6분 ~ 오전 10시 42분
-
 import java.util.*;
+/*
+[알고리즘]
+1. 배열 자르기(자른 배열 copy해서 저장)
+2. 자른 배열 정렬하기
+3. K번째에 있는 수 구하기
+* 참고 : commands의 형태 -> [i,j,k], [i,j,k], [i,j,k]
+*/
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+        //int[] answer = {};
         
-        //commands : [[i, j, k], [i, j, k], [i, j, k]] 형식
         
-        /*
-        1. 배열 자르기
-        2. 자른 배열 정렬하기
-        3. k에 해당하는 k번째 숫자 리턴하기(배열에 담아서 리턴해야 함)
-        */
+        int[] answer = new int[commands.length];
         
-        int[] result = new int[commands.length];
-        
-        for(int i = 0; i<commands.length; i++){
-            int[] command = commands[i]; //자르기 시작하는 부분만 확인할 수 있도록 command 추가
-                
-            //1. 배열 자르기
-            int[] cut = Arrays.copyOfRange(array, command[0] - 1, command[1]); //인텍스가 0부터 시작되는데, 프로그램에서는 1부터 인식하기 때문
-
+        for(int i=0; i<commands.length; i++){
+            //1. 배열 자르기(자른 배열 copy해서 저장)
+            int[] cut = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]); //주의: commands[i][0]은 인덱스 1의 위치이므로 -1을 해줘야 0부터 시작함!
+            
             //2. 자른 배열 정렬하기
             Arrays.sort(cut);
-
-            //3. k에 해당하는 k번째 숫자 리턴하기(배열에 담아서 리턴해야 함)
-            result[i] = cut[command[2] - 1];
+            
+            //3. K번째에 있는 수 구하기
+            answer[i] = cut[commands[i][2]-1]; //위의 주의와 같은 이유
         }
-        return result;   
+        return answer;
     }
 }
-
