57439cbf8d1114ab8d5e2b982adda36ed1e800c8
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-06-17 19:41:15 +0900

    [Silver III] Title: 퇴사, Time: 176 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,11 +1,13 @@
 import java.util.*;
+
 public class Main {
 
     static int N;
     static int[] T;
     static int[] P;
     static int[] d;
-    static int maxCon; //최대 수익 -> 전역변수로 위치 변경
+    static int maxCon;
+    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
@@ -16,15 +18,13 @@ public class Main {
             T[i] = sc.nextInt();
             P[i] = sc.nextInt();
         }
-        d = new int[N + 1]; //d 크기를 N+1로 변경
+        d = new int[N + 1];
 
         for(int i = N-1; i>=0; i--){
 
             int time = T[i] + i;
 
-            if(time<=N){ //상담이 되는 경우
-                //T[i]+i는 상담을 시작하는 날짜(T[i])부터 상담을 진행하는 데 걸리는 일 수(i)를 더한 값
-                //상담을 시작하고 나면 그 일 수만큼은 무조건 상담이 불가하기 때문!
+            if(time<=N){
                 d[i] = Math.max((P[i] + d[time]), maxCon);
                 maxCon = d[i];
             } else {
