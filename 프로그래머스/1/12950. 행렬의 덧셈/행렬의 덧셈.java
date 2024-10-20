275e02ad06e5df26198ae1702f238f45a3b01459
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2024-10-20 19:58:50 +0900

    [level 1] Title: 행렬의 덧셈, Time: 4.19 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/*알고리즘
+- 더한 결과 == 행 배열 + 열 배열
+*/
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
