e349f8d185257a5b36824d5a73823434c91efb11
Hayan Lee <102213509+hayannn@users.noreply.github.com>
2025-03-09 22:04:14 +0900

    feat: 443-String-Compression.java

---
+++ LeetCode/Medium/443.String-Compression/443-String-Compression.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int compress(char[] chars) {
+        int idx = 0, cnt = 1;
+        
+        for (int i = 1; i <= chars.length; i++) {
+            if (i < chars.length && chars[i] == chars[i - 1]) {
+                cnt++;
+            } else {
+                chars[idx++] = chars[i - 1];
+                if (cnt > 1) {
+                    for (char c : String.valueOf(cnt).toCharArray()) {
+                        chars[idx++] = c;
+                    }
+                }
+                cnt = 1;
+            }
+        }
+        return idx;
+    }
+}
